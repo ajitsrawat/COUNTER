21ddb9140c53a461dc9bc6bf3066d99958bda96d
Ajit Singh Rawat <ajitsrawat@yahoo.com>
2020-07-22 19:40:13 +0530

    New Files for Alphabet priting

---
+++ alphabetsPrinter.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import time
+
+print("Hello, I am going to print Alphabets")
+a = 0
+for i in range (0, 58):
+    a = 65 + i
+    print(chr(a))
+    time.sleep(2)
+    
